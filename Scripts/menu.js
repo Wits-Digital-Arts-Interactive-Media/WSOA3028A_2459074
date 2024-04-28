0f4e5fab1ad94543983f2570808bf384bc2317f3
DanzoLuvsKFC <131270351+DanzoLuvsKFC@users.noreply.github.com>
2024-04-29 01:11:58 +0200

    Java tings

---
+++ Scripts/menu.js
@@ -0,0 +1,27 @@
+
+
+const menuItems = [
+    {name: "Home", href:   "/index.html"},
+    {name: "Blog", href: "/Blogs/blog.html"},
+    {name: "Essays", href: "/Essays/essays.html"},
+    {name: "Portfolio", href: "/Portfolio/portfolio.html"},
+    {name: "Design", href: "/Design/design.html"},
+    {name: "About", href: "/aboutme.html"},
+]
+
+export function initialise(currentPage) {
+    console.log("it should work")
+    const nav = document.querySelector("header > nav")       //nav is when you use links so this is getting it from the first header it finds
+    const ul = document.createElement("ul")                  // ul is unordered list so this is creating it
+    for (let menuItem of menuItems) {                        //basically for loop assigning each item in  menuItems to menuItem for each iteration
+        const li = document.createElement("li")              //creates a list item 
+        if (currentPage != menuItem.name) {                  //compares the names of the current page and the name of the menuitem page
+            const a = document.createElement("a")            //a tag is for hyperlinks basically that href thing
+            a.innerText = menuItem.name                      //this is the word that comes highlighted in blue
+            a.setAttribute("href", menuItem.href)            //no clue what this does
+            li.appendChild(a)                                //this adds it as a child of the li object we made
+        } else { li.innerText = menuItem.name }
+        ul.appendChild(li)                                   //adds it to the unordered list we made at the beginning
+    }
+    nav.appendChild(ul)
+}
